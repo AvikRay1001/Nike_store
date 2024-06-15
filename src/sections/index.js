f5de360d07a8c75950a2d6f5bb7c10d1cacbb283
AvikRay1001 <avikray1001@gmail.com>
2024-06-15 22:19:03 +0530

    Day1

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SpecialOffers from "./SpecialOffers";
+import Services from "./Services";
+import Footer from "./Footer";
+import SuperQuality from "./SuperQuality";
+import Subscribe from "./Subscribe";
+import CustomerReviews from "./CustomerReviews";
+
+export {
+    Hero,
+    PopularProducts,
+    SpecialOffers,
+    Services,
+    Footer,
+    SuperQuality,
+    Subscribe,
+    CustomerReviews
+}
